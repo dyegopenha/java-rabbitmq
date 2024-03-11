d82e8e08af86e0502d5ae3017837d2e76dce4193
Dyego Penha <dyegopenha@hotmail.com>
2024-03-11 11:34:36 -0300

    add test

--- src/test/java/com/example/messagingrabbitmq/MessagingRabbitmqApplicationTests.java
+++ src/test/java/com/example/messagingrabbitmq/MessagingRabbitmqApplicationTests.java
@@ -1,13 +1,37 @@
 package com.example.messagingrabbitmq;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.jupiter.api.Test;
+
+import org.springframework.amqp.AmqpConnectException;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
-class MessagingRabbitmqApplicationTests {
+class MessagingRabbitmqApplicationTest {
+
+	@MockBean
+	private Runner runner;
+
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+
+	@Autowired
+	private Receiver receiver;
 
 	@Test
-	void contextLoads() {
+	public void test() throws Exception {
+		try {
+			rabbitTemplate.convertAndSend(AppConfig.queueName,
+					"Hello from RabbitMQ!");
+			receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+		}
+		catch (AmqpConnectException e) {
+			// ignore - rabbit is not running
+		}
 	}
 
 }
